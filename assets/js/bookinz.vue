589ac6131ca109d78e55036864b8e8ae26cd7e29
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-10 00:29:01 +0200

    empty bookingz component added

---
+++ assets/js/bookinz.vue
@@ -0,0 +1,38 @@
+<template>
+
+<div>
+  
+
+
+</div>
+
+</template>
+
+<script>
+
+    export default {
+        
+        data: function(){
+
+            return {
+            
+                
+
+            }
+
+        },
+
+        methods: {
+        
+
+        
+        }
+
+        ,mounted() {
+        
+
+
+        }
+    }
+
+</script>
