f6923dd04843a8bf1bbbc9a2ac4afc1bd64f82c3
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-10 06:31:00 +0200

    completed bdd

--- features/bookingz.feature
+++ features/bookingz.feature
@@ -5,18 +5,18 @@ Feature: Library Management System Loan Module
 
   Scenario: Accepted Loan
     Given the following books in the library
-    | title                          | code	   | startdate  | duedate    | timeextended |
-    | Programming Phoenix 13         | A       | 2019-01-10 | 2018-01-17 | 0            |
-    | Secret of the Javascript ninja | A       | 2018-12-19 | 2019-01-16 | 4            |
+    | title                          | code	   | start_date  | due_date    | num_of_extensions |
+    | Programming Phoenix 13         | A       | 2019-01-10  | 2019-01-17  | 0                 |
+    | Secret of the Javascript ninja | A       | 2018-12-19  | 2019-01-16  | 4                 |
     And I want to extend the loan period of "1" book from my borrowed books
     When I extend the loan period for this book
     Then I should get an accepted message for succesfull loan
 
   Scenario: Rejected Loan
       Given the following books in the library
-      | title                          | code	   | startdate  | duedate    | timeextended |
-      | Programming Phoenix 13         | A       | 2019-01-10 | 2019-01-17 | 0            |
-      | Secret of the Javascript ninja | A       | 2018-12-19 | 2019-01-16 | 4            |
+      | title                          | code	   | start_date  | due_date    | num_of_extensions |
+      | Programming Phoenix 13         | A       | 2019-01-10  | 2019-01-17  | 0                 |
+      | Secret of the Javascript ninja | A       | 2018-12-19  | 2019-01-16  | 4                 |
       And I want to extend the loan period of "2" book from my borrowed books
       When I extend the loan period for this book
-      Then I should get a rejected message for succesfull loan
+      Then I should get a rejected message for unsuccesfull loan
--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -1,5 +1,6 @@
 defmodule WhiteBreadContext do
   use WhiteBread.Context
+  use Hound.Helpers
 
   alias Bookingz.{Repo, Book}
 
@@ -17,28 +18,45 @@ defmodule WhiteBreadContext do
 
   scenario_finalize fn _status, _state ->
     Ecto.Adapters.SQL.Sandbox.checkin(Repo)
-    Hound.end_session
+    #Hound.end_session
     nil
   end
 
-  given_ ~r/^the following books in the library$/, fn state ->
-    {:ok, state}
+  given_ ~r/^the following books in the library$/, fn state, %{table_data: table} ->
+    table
+    |>  Enum.each(fn book ->
+        Repo.insert!(Book.changeset(
+                      %Book{},
+                      %{
+                        title: book.title,
+                        code: book.code,
+                        start_date: book.start_date,
+                        due_date: book.due_date,
+                        num_of_extensions: book.num_of_extensions
+                      })) end)
+
+        {:ok, state}
   end
 
   and_ ~r/^I want to extend the loan period of "(?<argument_one>[^"]+)" book from my borrowed books$/,
-  fn state, %{argument_one: _argument_one} ->
-    {:ok, state}
+  fn state, %{argument_one: argument_one} ->
+    navigate_to "/#/"
+    {:ok, state |> Map.put(:item_id, argument_one)}
   end
 
   when_ ~r/^I extend the loan period for this book$/, fn state ->
+    IO.puts state[:item_id]
+    click({:id, state[:item_id]})
     {:ok, state}
   end
 
   then_ ~r/^I should get an accepted message for succesfull loan$/, fn state ->
+    assert visible_in_page? ~r/#{"Book loan extension has been accepted"}/
     {:ok, state}
   end
 
-  then_ ~r/^I should get a rejected message for succesfull loan$/, fn state ->
+  then_ ~r/^I should get a rejected message for unsuccesfull loan$/, fn state ->
+    assert visible_in_page? ~r/#{"Book loan extension has been rejected"}/
     {:ok, state}
   end
 
