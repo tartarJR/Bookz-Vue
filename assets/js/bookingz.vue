fa3c1f903fb91d42e5dcb7a52f1946b22422c3ae
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-10 02:45:13 +0200

    vue, displaying loans feature

--- assets/js/bookingz.vue
+++ assets/js/bookingz.vue
@@ -2,7 +2,55 @@
 
 <div>
   
+    <h3>Book Loans</h3>
 
+    <p> {{ this.responseMessage }} </p>
+
+    <table class="table">
+
+        <thead>
+
+            <tr>
+
+                <th>Title</th>
+
+                <th>Start Date</th>
+
+                <th>Due Date</th>
+
+                <th>Number Of Extensions</th>
+
+                <th>Action</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody id="books">
+
+            <tr v-for="(book, i) in books" :key="book.id">
+
+                <td> {{ book.title }} </td>
+
+                <td> {{ book.start_date }} </td>
+
+                <td> {{ book.due_date }} </td>
+
+                <td> {{ book.num_of_extensions }} </td>
+
+                <td>
+                    <a :id="i + 1" v-if="book.should_show_action" v-on:click="update(book.id)">Extend Loan</a>
+                    <label v-else>Loan Expired</label>
+                </td>
+
+            </tr>
+
+        </tbody>
+
+    </table>
+
+    Display only ongoing book loans
+    <input type="checkbox" id="all" v-model="shouldShowAllBooks" v-on:click="reset" />
 
 </div>
 
@@ -10,13 +58,19 @@
 
 <script>
 
+    import axios from "axios";
+
     export default {
         
         data: function(){
 
             return {
             
-                
+                shouldShowAllBooks: true,
+
+                books: [],
+
+                responseMessage: ""
 
             }
 
@@ -24,13 +78,37 @@
 
         methods: {
         
+            getBookLoans: function(shouldShowAllBooks) {
+            
+                axios.post("/api/book-loans", { should_show_all_books: shouldShowAllBooks })
+                    .then(response => {
 
+                      this.books = response.data.books
+                      
+                    }).catch(error => {
+                        
+                        console.log(error);
+
+                    });
+
+            },
+
+            update: function(id) {
+
+
+            },
+
+            reset: function() {
+
+                this.getBookLoans(this.shouldShowAllBooks)
+
+            }
         
-        }
+        },
 
-        ,mounted() {
+        mounted() {
         
-
+            this.getBookLoans(false)
 
         }
     }
